f42bc886b5fcb99e2a4d498800acd7d47d811ffb
Rohith Chandu <rohithchandu@gmail.com>
2024-04-30 13:09:58 -0400

    Added basic application properties(server port, db configs) and modified code in the Foo.java file (changed to App.java)

---
+++ src/main/java/dev/codescreen/App.java
@@ -0,0 +1,18 @@
+package dev.codescreen;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Application Class. Entry point to Spring Boot application
+ */
+public class App {
+
+  /**
+   *
+   *
+   * @return
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
